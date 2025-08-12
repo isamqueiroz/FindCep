0388b59fc6e9f8fc0d25464054c989bc5095d57b
Isadora Macedo <isa22queiroz@gmail.com>
2025-08-12 15:04:21 -0300

    :tada: Enviando projeto com a tela de consulta em desenvolvimento

---
+++ findcep/app/index.tsx
@@ -0,0 +1,53 @@
+import { Text, View, StyleSheet, Image,  ImageBackground,} from "react-native";
+import {Input} from '../components/input/Input'
+export default function Index() {
+  return (
+<>
+      {/* 1. Logo + imagem de fundo */}
+      <ImageBackground source={ require('../assets/images/ImagemFundo.png')}
+      style={styles.ImagemFundo}>
+        <Image source={require('../assets/images/LogoFindCEP.png')} style={styles.logo}></Image>
+
+      </ImageBackground>
+
+      {/* 2. Campo de consulta */}
+      <View style={styles.container}>
+
+      {/* 2.1. Título */}
+      <Text style={styles.titulo}>Consulte seu CEP</Text>
+
+      {/* 2.2. Input */}
+      <Input/>
+      {/* 2.3. Botão */}
+      {/* 2.3. Botão */}
+
+      </View>
+</>
+  );
+}
+
+//Estilos dos meus componentes:
+const styles = StyleSheet.create({
+ ImagemFundo : {
+  flex: 1,
+ justifyContent: 'center',
+ alignItems : 'center',
+ width: '100%',
+ height: '100%'
+ },
+ logo: {
+  width: 100,
+  height : 120
+ },
+ container: {
+ flex: 1.5,
+ alignItems: 'center',
+paddingTop: 50,
+paddingBottom: 50,
+gap: 40
+ },
+ titulo: {
+ fontSize: 25,
+
+ }
+})
